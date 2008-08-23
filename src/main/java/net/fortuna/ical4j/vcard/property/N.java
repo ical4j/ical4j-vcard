89a82e3c0dc1fb3505c88765a039ac0f9223a781
fortuna <fortuna@users.sourceforge.net>
2008-08-23 05:57:39 +0000

    Added identification properties

---
+++ src/main/java/net/fortuna/ical4j/vcard/property/N.java
@@ -0,0 +1,131 @@
+/*
+ * $Id$
+ *
+ * Created on 23/08/2008
+ *
+ * Copyright (c) 2008, Ben Fortuna
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  o Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ *  o Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ *  o Neither the name of Ben Fortuna nor the names of any other contributors
+ * may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.fortuna.ical4j.vcard.property;
+
+import net.fortuna.ical4j.vcard.Property;
+
+import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * @author Ben
+ *
+ */
+public final class N extends Property {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1117450875931318523L;
+
+    private String familyName;
+
+    private String givenName;
+
+    private String[] additionalNames;
+
+    private String[] prefixes;
+
+    private String[] suffixes;
+    
+    /**
+     * @param familyName
+     * @param givenName
+     * @param additionalNames
+     * @param prefixes
+     * @param suffixes
+     */
+    public N(String familyName, String givenName, String[] additionalNames, String[] prefixes, String[] suffixes) {
+        super(Name.N);
+        this.familyName = familyName;
+        this.givenName = givenName;
+        this.additionalNames =additionalNames;
+        this.prefixes = prefixes;
+        this.suffixes = suffixes;
+    }
+    
+    /* (non-Javadoc)
+     * @see net.fortuna.ical4j.vcard.Property#getValue()
+     */
+    @Override
+    public String getValue() {
+        StringBuilder b = new StringBuilder();
+        if (StringUtils.isNotEmpty(familyName)) {
+            b.append(familyName);
+        }
+        if (StringUtils.isNotEmpty(givenName)) {
+            if (b.length() > 0) {
+                b.append(';');
+            }
+            b.append(givenName);
+        }
+        if (!ArrayUtils.isEmpty(additionalNames)) {
+            if (b.length() > 0) {
+                b.append(';');
+            }
+            for (int i = 0; i < additionalNames.length; i++) {
+                if (i > 0) {
+                    b.append(',');
+                }
+                b.append(additionalNames[i]);
+            }
+        }
+        if (!ArrayUtils.isEmpty(prefixes)) {
+            if (b.length() > 0) {
+                b.append(';');
+            }
+            for (int i = 0; i < prefixes.length; i++) {
+                if (i > 0) {
+                    b.append(',');
+                }
+                b.append(prefixes[i]);
+            }
+        }
+        if (!ArrayUtils.isEmpty(suffixes)) {
+            if (b.length() > 0) {
+                b.append(';');
+            }
+            for (int i = 0; i < suffixes.length; i++) {
+                if (i > 0) {
+                    b.append(',');
+                }
+                b.append(suffixes[i]);
+            }
+        }
+        return b.toString();
+    }
+
+}
