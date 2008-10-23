8acdea323efcaa2a70e0f01b9f9ecbe27851c182
fortuna <fortuna@users.sourceforge.net>
2008-10-23 12:15:48 +0000

    Added security and calendar properties

---
+++ src/main/java/net/fortuna/ical4j/vcard/property/Key.java
@@ -0,0 +1,114 @@
+/*
+ * $Id$
+ *
+ * Created on 23/10/2008
+ *
+ * Copyright (c) 2008, Ben Fortuna
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  o Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ *  o Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ *  o Neither the name of Ben Fortuna nor the names of any other contributors
+ * may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.fortuna.ical4j.vcard.property;
+
+import net.fortuna.ical4j.vcard.Property;
+import net.fortuna.ical4j.vcard.parameter.Encoding;
+import net.fortuna.ical4j.vcard.parameter.Type;
+
+import org.apache.commons.codec.BinaryEncoder;
+import org.apache.commons.codec.EncoderException;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * @author Ben
+ *
+ */
+public class Key extends Property {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -6645173064940148955L;
+
+    private String value;
+    
+    private byte[] binary;
+
+    private Log log = LogFactory.getLog(Key.class);
+    
+    /**
+     * @param value
+     */
+    public Key(String value) {
+        super(Name.KEY);
+        this.value = value;
+    }
+    
+    /**
+     * @param binary
+     */
+    public Key(byte[] binary) {
+        this(binary, null);
+    }
+    
+    /**
+     * @param binary
+     * @param contentType
+     */
+    public Key(byte[] binary, Type contentType) {
+        super(Name.KEY);
+        this.binary = binary;
+        getParameters().add(Encoding.B);
+        if (contentType != null) {
+            getParameters().add(contentType);
+        }
+    }
+    
+    /* (non-Javadoc)
+     * @see net.fortuna.ical4j.vcard.Property#getValue()
+     */
+    @Override
+    public String getValue() {
+        if (StringUtils.isNotEmpty(value)) {
+            return value;
+        }
+        else if (binary != null) {
+            try {
+                BinaryEncoder encoder = new Base64();
+                return new String(encoder.encode(binary));
+            }
+            catch (EncoderException ee) {
+                log.error("Error encoding binary data", ee);
+            }
+        }
+        return null;
+    }
+
+}
