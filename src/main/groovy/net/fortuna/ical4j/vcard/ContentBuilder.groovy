10c3af9f90c0ae2a3c8bcb501927bebaec2840e1
fortuna <fortuna@users.sourceforge.net>
2009-08-02 05:54:50 +0000

    Added property factories

--- src/main/groovy/net/fortuna/ical4j/vcard/ContentBuilder.groovy
+++ src/main/groovy/net/fortuna/ical4j/vcard/ContentBuilder.groovy
@@ -32,7 +32,11 @@
 package net.fortuna.ical4j.vcard
 
 import groovy.util.FactoryBuilderSupportimport net.fortuna.ical4j.vcard.parameter.EncodingFactoryimport net.fortuna.ical4j.vcard.property.PhotoFactoryimport net.fortuna.ical4j.vcard.parameter.ValueFactoryimport net.fortuna.ical4j.vcard.property.VersionFactoryimport net.fortuna.ical4j.vcard.property.FnFactoryimport net.fortuna.ical4j.vcard.property.NFactory
-import net.fortuna.ical4j.vcard.parameter.LanguageFactoryimport net.fortuna.ical4j.vcard.parameter.PidFactoryimport net.fortuna.ical4j.vcard.parameter.PrefFactoryimport net.fortuna.ical4j.vcard.parameter.TypeFactory/**
+import net.fortuna.ical4j.vcard.parameter.LanguageFactoryimport net.fortuna.ical4j.vcard.parameter.PidFactoryimport net.fortuna.ical4j.vcard.parameter.PrefFactoryimport net.fortuna.ical4j.vcard.parameter.TypeFactoryimport net.fortuna.ical4j.vcard.property.UrlFactoryimport net.fortuna.ical4j.vcard.property.UidFactoryimport net.fortuna.ical4j.vcard.property.TzFactoryimport net.fortuna.ical4j.vcard.property.TitleFactoryimport net.fortuna.ical4j.vcard.property.TelephoneFactoryimport net.fortuna.ical4j.vcard.property.SourceFactoryimport net.fortuna.ical4j.vcard.property.SoundFactoryimport net.fortuna.ical4j.vcard.property.SortStringFactoryimport net.fortuna.ical4j.vcard.property.RoleFactory
+import net.fortuna.ical4j.vcard.property.RevisionFactory
+import net.fortuna.ical4j.vcard.property.RelatedFactory
+import net.fortuna.ical4j.vcard.property.*
+/**
  * $Id$
  *
  * Created on: 02/08/2009
@@ -45,9 +49,46 @@ public class ContentBuilder extends FactoryBuilderSupport {
     public ContentBuilder() {
         registerFactory('vcard', new VCardFactory())
         // properties..
+        registerFactory('address', new AddressFactory())
+        registerFactory('agent', new AgentFactory())
+        registerFactory('bday', new BDayFactory())
+        registerFactory('birth', new BirthFactory())
+        registerFactory('caladruri', new CalAdrUriFactory())
+        registerFactory('caluri', new CalUriFactory())
+        registerFactory('categories', new CategoriesFactory())
+        registerFactory('class', new ClazzFactory())
+        registerFactory('dday', new DDayFactory())
+        registerFactory('death', new DeathFactory())
+        registerFactory('email', new EmailFactory())
+        registerFactory('fburl', new FbUrlFactory())
         registerFactory('fn', new FnFactory())
+        registerFactory('gender', new GenderFactory())
+        registerFactory('geo', new GeoFactory())
+        registerFactory('impp', new ImppFactory())
+        registerFactory('key', new KeyFactory())
+        registerFactory('kind', new KindFactory())
+        registerFactory('label', new LabelFactory())
+        registerFactory('lang', new LangFactory())
+        registerFactory('logo', new LogoFactory())
+        registerFactory('member', new MemberFactory())
+        registerFactory('name', new NameFactory())
         registerFactory('n', new NFactory())
+        registerFactory('nickname', new NicknameFactory())
+        registerFactory('note', new NoteFactory())
+        registerFactory('org', new OrgFactory())
         registerFactory('photo', new PhotoFactory())
+        registerFactory('prodid', new ProdIdFactory())
+        registerFactory('related', new RelatedFactory())
+        registerFactory('revision', new RevisionFactory())
+        registerFactory('role', new RoleFactory())
+        registerFactory('sortString', new SortStringFactory())
+        registerFactory('sound', new SoundFactory())
+        registerFactory('source', new SourceFactory())
+        registerFactory('telephone', new TelephoneFactory())
+        registerFactory('title', new TitleFactory())
+        registerFactory('tz', new TzFactory())
+        registerFactory('uid', new UidFactory())
+        registerFactory('url', new UrlFactory())
         registerFactory('version', new VersionFactory())
         // parameters..
         registerFactory('encoding', new EncodingFactory())
