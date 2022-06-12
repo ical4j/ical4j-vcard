8f73677b22621ef9df108f2205a5e10d3cb67305
Ben Fortuna <bfortuna@odecee.com.au>
2022-06-12 18:37:28 +1000

    Refactored immutable properties to avoid potential deadlock

--- src/main/java/net/fortuna/ical4j/vcard/property/immutable/ImmutableVersion.java
+++ src/main/java/net/fortuna/ical4j/vcard/property/immutable/ImmutableVersion.java
@@ -1,5 +1,7 @@
 package net.fortuna.ical4j.vcard.property.immutable;
 
+import net.fortuna.ical4j.model.Parameter;
+import net.fortuna.ical4j.model.Property;
 import net.fortuna.ical4j.model.property.immutable.ImmutableProperty;
 import net.fortuna.ical4j.vcard.property.Version;
 
@@ -12,5 +14,30 @@ public class ImmutableVersion extends Version implements ImmutableProperty {
 
     public ImmutableVersion(String value) {
         super(value);
+    }
+
+    @Override
+    public <T extends Property> T add(Parameter parameter) {
+        return ImmutableProperty.super.add(parameter);
+    }
+
+    @Override
+    public <T extends Property> T remove(Parameter parameter) {
+        return ImmutableProperty.super.remove(parameter);
+    }
+
+    @Override
+    public <T extends Property> T removeAll(String... parameterName) {
+        return ImmutableProperty.super.removeAll(parameterName);
+    }
+
+    @Override
+    public <T extends Property> T replace(Parameter parameter) {
+        return ImmutableProperty.super.replace(parameter);
+    }
+
+    @Override
+    public void setValue(final String aValue) {
+        ImmutableProperty.super.setValue(aValue);
     }
 }
