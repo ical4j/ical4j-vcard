9b2dd897cb35878eac89ac63807c93c662cb12df
Ben Fortuna <fortuna@micronode.com>
2024-02-22 12:27:41 +1100

    Updated module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,19 @@
 module ical4j.vcard {
     requires java.base;
-    requires org.mnode.ical4j.core;
-    requires org.apache.commons.logging;
+
+    requires ical4j.core;
+
     requires org.apache.commons.codec;
+    requires org.apache.commons.logging;
+    requires org.apache.commons.io;
+    requires org.apache.commons.lang3;
+
+    // optional dependencies..
+    requires static org.codehaus.groovy;
 
     exports net.fortuna.ical4j.vcard;
     exports net.fortuna.ical4j.vcard.property;
+    exports net.fortuna.ical4j.vcard.property.immutable;
     exports net.fortuna.ical4j.vcard.filter;
     exports net.fortuna.ical4j.vcard.parameter;
 }
